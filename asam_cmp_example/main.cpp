f4c9ade7db432ee980dc01b0236f93565310659d
Aleksandr Kuzin <aleksandr.kuzin@promwad.com>
2024-09-09 14:57:10 +0200

    demo app rework

--- asam_cmp_example/main.cpp
+++ asam_cmp_example/main.cpp
@@ -11,80 +11,13 @@
 
 using namespace daq;
 
-class BufferFb : public FunctionBlock
-{
-public:
-    explicit BufferFb(const ContextPtr& ctx, const ComponentPtr& parent, const StringPtr& localId)
-        : FunctionBlock(CreateType(), ctx, parent, localId)
-    {
-        createInputPorts();
-    }
-
-    void onPacketReceived(const InputPortPtr& port) override
-    {
-        std::scoped_lock lock(sync);
-
-        const auto conn = port.getConnection();
-        if (!conn.assigned())
-            return;
-
-        auto packet = conn.dequeue();
-        while (packet.assigned())
-        {
-            const auto packetType = packet.getType();
-            if (packetType == PacketType::Event)
-            {
-
-            }
-            else if (packetType == PacketType::Data)
-            {
-                DataPacketPtr dataPacket = packet.asPtr<IDataPacket>();
-                const auto domainPacket = dataPacket.getDomainPacket();
-                if (domainPacket.assigned())
-                {
-                    if (dataPacket.getDataDescriptor().getSampleType() == SampleType::Struct)
-                    {
-                        std::cout << "Received packet: " + dataPacket.getDataDescriptor().getName().toStdString() +
-                            ", samplesCount: " + std::to_string(dataPacket.getSampleCount()) << std::endl;
-                    }
-                }
-            }
-
-            packet = conn.dequeue();
-        }
-    }
-
-    
-    FunctionBlockTypePtr CreateType()
-    {
-        return FunctionBlockType("BufferFb", "Buffer", "Register received packets");
-    }
-
-    void createInputPorts()
-    {
-        inputPort = createAndAddInputPort("voltage", PacketReadyNotification::Scheduler);
-    }
-
-private:
-    InputPortPtr inputPort;
-};
-
 int main()
 {
     const auto instance = Instance();
-
     const auto device = instance.addDevice("daqref://device0");
-    device.setPropertyValue("EnableCANChannel", True);
-    const ChannelPtr canChannel = device.getChannels(search::Custom([](const ComponentPtr& comp) { return comp.getLocalId() == "refcanch"; }))[0];
-    const auto canSignal = canChannel.getSignals()[0];
-
-    auto asamCmpDataSink = instance.addFunctionBlock("asam_cmp_data_sink_module");
-
-
-    instance.removeFunctionBlock(asamCmpDataSink);
 
     auto asamCmpCaptureModule = instance.addFunctionBlock("asam_cmp_capture_module_fb");
-    asamCmpDataSink = instance.addFunctionBlock("asam_cmp_data_sink_module");
+    auto asamCmpDataSink = instance.addFunctionBlock("asam_cmp_data_sink_module");
 
     if (asamCmpCaptureModule.getPropertySelectionValue("NetworkAdapters") != asamCmpDataSink.getPropertySelectionValue("NetworkAdapters"))
     {
@@ -97,34 +30,42 @@ int main()
     ProcedurePtr createItfProc = captureFb.getPropertyValue("AddInterface");
     createItfProc();
     auto itf = captureFb.getFunctionBlocks()[0];
-    itf.setPropertyValue("PayloadType", 1);
+    itf.setPropertyValue("PayloadType", 3);
 
     ProcedurePtr createStreamProc = itf.getPropertyValue("AddStream");
     createStreamProc();
-    auto streamFb = itf.getFunctionBlocks()[0];
+    createStreamProc();
 
-    InputPortPtr streamInputPort = streamFb.getInputPorts().getItemAt(0);
-    streamInputPort.connect(canSignal);
+    for (int i = 0; i < 2; ++i)
+    {
+        auto streamFb = itf.getFunctionBlocks()[i];
+        InputPortPtr streamInputPort = streamFb.getInputPorts().getItemAt(0);
+        streamInputPort.connect(device.getChannels()[i].getSignals()[0]);
+    }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
     auto dataSinkFb =
         asamCmpDataSink.getFunctionBlocks(search::Custom([](const ComponentPtr& comp) { return comp.getLocalId() == "asam_cmp_data_sink"; }))[0];
     ProcedurePtr buildFromStatusProc = dataSinkFb.getPropertyValue("AddCaptureModuleFromStatus");
 
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
     buildFromStatusProc(0);
 
     auto sinkCaptureFb = dataSinkFb.getFunctionBlocks()[0];
     auto sinkItf = sinkCaptureFb.getFunctionBlocks()[0];
-    auto sinkStreamFb = sinkItf.getFunctionBlocks()[0];
 
-    auto sinkSignal = sinkStreamFb.getSignals()[0];
+    FunctionBlockPtr renderer = instance.addFunctionBlock("RefFBModuleRenderer");
+    renderer.setPropertyValue("ShowLastValue", true);
+    renderer.setPropertyValue("UseCustomMinMaxValue", true);
+    for (int i = 0; i < 2; ++i)
+    {
+        renderer.getInputPorts()[i].connect(sinkItf.getFunctionBlocks()[i].getSignals()[0]);
+    }
 
-    auto buffer = createWithImplementation<IFunctionBlock, BufferFb>(instance.getContext(), nullptr, "buffer_fb");
-
-    auto bufferInputPort = buffer.getInputPorts()[0];
-
-    bufferInputPort.connect(sinkSignal);
+    for (int i = 0; i < 2; ++i)
+    {
+        renderer.getInputPorts()[i + 2].connect(device.getChannels()[i].getSignals()[0]);
+    }
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 
